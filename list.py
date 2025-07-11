2f1b39f48c3c81f4d442e4ae6aa3d2f58b0432f2
sarina365 <sarinamagar81@gmail.com>
2025-07-11 12:38:49 +0545

    assign

---
+++ list.py
@@ -0,0 +1,57 @@
+# L1 = [1,2,7,8,15,21]
+
+# L1.sort()
+# print(L1)
+
+# L1.reverse()
+# print(L1)
+
+# L1.append(8)
+# print(L1)
+
+# L1.insert(3, 8)
+# print(L1)
+
+# last_item = L1.pop
+# print(last_item)
+# print(L1)
+
+# L1.append(8)
+# last_item = L1.pop()
+# print(last_item)
+# print(L1)
+
+#wap to seven fruits in a list entered by the user
+#wap to accept marks of fixed students and display them in a sorted manner.
+#wap to sum a list with 4 numbers.
+
+# fruits = []
+
+# for i in range(7):
+#     fruit = input(f"Enter fruit {i+1}: ")
+#     fruits.append(fruit)
+
+# print("The list of fruits is:", fruits)
+
+
+
+# marks = []
+
+# for i in range(6):
+#     mark = int(input(f"Enter marks for student {i+1}: "))
+#     marks.append(mark)
+
+# marks.sort()  # Sort the list in ascending order
+
+# print("Sorted marks are:", marks)
+
+
+# numbers = []
+
+# for i in range(4):
+#     num = float(input(f"Enter number {i+1}: "))
+#     numbers.append(num)
+
+# total = sum(numbers)
+
+# print("The sum of the numbers is:", total)
