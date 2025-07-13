1fb81e3c28bdcba9958d61f2f69ad1112504ddd7
sarina365 <sarinamagar81@gmail.com>
2025-07-13 11:30:27 +0545

    assign

---
+++ project.py
@@ -0,0 +1,23 @@
+# Simple Calculator
+
+print("Select operation:")
+print("1. Add (+)")
+print("2. Subtract (-)")
+print("3. Multiply (*)")
+
+choice = input("Enter choice (1/2/3): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    result = num1 + num2
+    print("Result: ", result)
+elif choice == '2':
+    result = num1 - num2
+    print("Result: ", result)
+elif choice == '3':
+    result = num1 * num2
+    print("Result: ", result)
+else:
+    print("Invalid input")
